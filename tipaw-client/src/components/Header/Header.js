ebf646902dbc782acfc6e9cc33507ea65d95c1a9
timothyk0908 <timothyk0908@gmail.com>
2020-06-19 13:55:19 +0200

    intialization of both projects, created contact endpoint in express and (very) basic header to test

---
+++ tipaw-client/src/components/Header/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Header.css';
+
+function Header() {
+    return (
+        <div>
+            <div class="tipaw-header">
+                <div class="header-content">
+                    <h1>Tipaw Header</h1>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Header;
