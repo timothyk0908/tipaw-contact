d1ddb3f365c205c6b43c0ce1b90dfe19c26d5ada
timothyk0908 <timothyk0908@gmail.com>
2020-06-21 15:12:32 +0200

    finished mimicking header, installed react-bootstrap for quick styling

--- tipaw-client/src/App.js
+++ tipaw-client/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header/Header.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
--- tipaw-client/src/components/Header/Header.js
+++ tipaw-client/src/components/Header/Header.js
@@ -1,15 +1,28 @@
 import React from 'react';
+import { Button, Image, Nav, Navbar } from 'react-bootstrap';
+import { BrowserRouter as Router, Link } from "react-router-dom";
 import './Header.css';
+
 
 function Header() {
     return (
-        <div>
-            <div class="tipaw-header">
-                <div class="header-content">
-                    <h1>Tipaw Header</h1>
-                </div>
-            </div>
-        </div>
+        <Navbar bg="light" expand="lg">
+            <Navbar.Brand href="#home"><Image src="assets/logo.png" className="tipaw-logo" />
+                <span className="tipaw-name">TIPAW</span>
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="ml-auto">
+                    <Router>
+                        <Link to="/contact"><Nav.Link>Contact</Nav.Link></Link>
+                        <Link to="/adopt"> <Nav.Link>Chiens à adopter </Nav.Link></Link>
+                        <Link to="/sell">  <Nav.Link>Chiens à vendre</Nav.Link></Link>
+                        <Link to="/vets">  <Nav.Link> Vétérinaires</Nav.Link></Link>
+                        <Link to="/my-tipaw"> <Button className="my-tipaw" variant="warning">Mon Tipaw</Button></Link>
+                    </Router>
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar>
     );
 }
 
